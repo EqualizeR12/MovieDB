9023f909549eba80f5a8dcebefee8a8474cda34f
Alex <alex.fursal_m@outlook.com>
2022-05-15 13:22:58 +0300

    add Some

--- 035 Async, defer, динамические скрипты/111.js
+++ 035 Async, defer, динамические скрипты/111.js
@@ -1,4 +1,21 @@
-"use strict";
+'use strict';
 
 const p = document.querySelectorAll('p');
 console.log(p);
+
+// const script = document.createElement('script');
+// script.src = "test.js";
+// script.async = false;
+// document.body.append(script);
+//test.js добавляется после 111.js. Динамически загружаемые скрипты ведут себя как async
+//Изменить относительный порядок скриптов при их загрузке - script.async = false;
+
+function loadScript(src) {
+  const script = document.createElement('script');
+  script.src = src;
+  script.async = false;
+  document.body.append(script);
+}
+loadScript("test.js");
+loadScript("some.js");
+//Выполнение строго друг за другом
